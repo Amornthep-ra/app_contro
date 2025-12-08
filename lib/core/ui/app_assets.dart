32e2fee48c60fa42ad766dba83863c91c3c0a5cb
Amornthep.ra <amornthep064@gmail.com>
2025-12-08 12:21:50 +0700

    แก้รูป

--- lib/core/ui/app_assets.dart
+++ lib/core/ui/app_assets.dart
@@ -1,8 +1,8 @@
 // lib\core\ui\app_assets.dart
 class AppAssets {
-  static const String logoPrinceBot = 'assets/Logo/PrinceBot_Logo.png';
+  static const String logoPrinceBot = 'assets/Logo/logo_corner.png';
 
-  static const String cornerLogo = 'assets/Logo/PrinceBot_Logo.png';
+  static const String cornerLogo = 'assets/Logo/logo_corner.png';
 
   static const String menuGamepad8   = 'assets/icons/gamepad.png';
   static const String menuGamepad4   = 'assets/icons/gamepad.png';
